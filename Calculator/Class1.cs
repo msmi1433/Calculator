5ac4f8f10568a21107aec587eaa6d2773ced5c87
Myles <mylesbsmith96@gmail.com>
2024-03-08 16:23:33 +0000

    adds multiplication and division

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -4,5 +4,9 @@
     {
         public static int Add(int x, int y) => x + y;
         public static int Subtract(int x, int y) => x - y;
+
+        public static int Multiply(int x, int y) => x * y;
+
+        public static int Divide(int x, int y) => x / y;
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using NuGet.Frameworks;
 using Sandbox;
 
 namespace Tests
@@ -26,6 +27,29 @@ namespace Tests
         public void SubtractFailingTest()
         {
             Assert.That(Calculator.Subtract(4, 3), Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void MultiplyPassingTest()
+        {
+            Assert.That(Calculator.Multiply(3, 1), Is.EqualTo(3));
+        }
+        
+        [Test]
+        public void MultiplyFailingTest()
+        {
+            Assert.That(Calculator.Multiply(3, 1), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void DividePassingTest()
+        {
+            Assert.That(Calculator.Divide(6, 2), Is.EqualTo(3));
+        }
+        [Test]
+        public void DivideFailingTest()
+        {
+            Assert.That(Calculator.Divide(6, 2), Is.EqualTo(5));
         }
     }
 }
